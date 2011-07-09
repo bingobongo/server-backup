0a09a7596ab89f407595858d5c7e10e7e8920e79
Markus Mayer <onaneerde@doogvaard.net>
2011-07-09 12:15:55 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,37 @@
+Automated, rotating server backups to Strongspace
+=================================================
+
+Kudos to **ExpanDrive**
+
+- [Password-less login to Strongspace with SSH Keys](https://www.strongspace.com/help/password-less-login-with-ssh-keys)
+- [Type less with a SSH Alias](https://www.strongspace.com/help/ssh-alias)
+- [Automated Offsite MySQL backup to Strongspace](https://www.strongspace.com/help/automated-offsite-mysql-backup)
+
+and **Joyent**
+
+- [Automated backups to Strongspace](http://oldwiki.joyent.com/shared:automated-backups)
+
+
+Setup
+-----
+
+1. Create the directory where to store database dumps and the backup script on your server; set its permissons to `700`; e.g.
+
+		% mkdir -m 700 ~/backup
+
+2. Create the directories that will be used for the backup rotation purpose on Strongspace; they must all be in that same Space.
+
+3. Set up [password-less logins to Strongspace](https://www.strongspace.com/help/password-less-login-with-ssh-keys); set the permissions of the keys to `600`.
+
+4. Create an SSH alias as described [here](https://www.strongspace.com/help/ssh-alias) – like `config` file.
+
+5. Configure `backup.sh` and `exclude.txt`. Instructions on configuration are found inside `backup.sh`. Upload the two files to the before created directory on your server. Set the permissons of `backup.sh` to `700` and `exclude.txt` to `600`.
+
+6. Test the script and check that it runs okay.
+
+		% ~/backup/backup.sh
+
+7. Finally, add it as a cronjob.
+	- On a Joyent **SmartMachine** log in to Webmin and navigate to <b>Webmin</b> – <b>System</b> – <b>Scheduled Cron Jobs</b>. There hit <b>Create a new scheduled cron job</b>. In the input box labelled <b>Execute cron job as</b> put `root` and in the input box labelled <b>Command</b> put the path to your backup script (e.g. `/home/USERNAME/backup/backup.sh`). Finally, set when you want it to execute and hit <b>Create</b> to finish.
+
+	- On a Joyent **Shared SmartMachine** log in to Virtualmin and navigate to <b>Webmin Modules</b> – <b>Scheduled Cron Jobs</b>. There hit <b>Create a new scheduled cron job</b>. In the input box labelled <b>Command</b> put the path to your backup script (e.g. `/users/home/USERNAME/backup/backup.sh`). Finally, set when you want it to execute and hit <b>Create</b> to finish.
---
+++ backup.sh
@@ -0,0 +1,97 @@
+#!/bin/sh
+
+
+# CONFIGURATION SECTION
+# ---------------------
+
+# The primary user name that is used to login to the
+# (Shared) SmartMachine.
+
+JOY=username
+
+
+# The MySQL user name, password and a space-separated
+# list of MySQL database names to backup.
+
+MY_UN=db-username
+MY_PW=db-password
+MY_DB='db-name-1 db-name-2'
+
+
+# The Strongspace SSH alias name
+# (i.e. the before defined Host in “.ssh/config file”).
+
+SS_ALIAS=ss
+
+
+# The base directory plus its five subdirectory names
+# (used for the rotation purpose) under which to store
+# the backup on Strongspace.
+
+SS_BASE=/strongspace/username/space-name
+SS_NAME_1=subdirectory-name-1
+SS_NAME_2=subdirectory-name-2
+SS_NAME_3=subdirectory-name-3
+SS_NAME_4=subdirectory-name-4
+SS_NAME_5=subdirectory-name-5
+
+
+# How many days to keep the database backups for;
+# e.g. for a backup once a week and wanting to keep
+# eight backups on the disk, set this to “56”.
+
+MTIME=56
+
+
+# DON’T EDIT TOO MUCH BELOW HERE, UNLESS ONE IS DOING SOMETHING CUSTOM
+# --------------------------------------------------------------------
+
+
+# Define pathes (i.e. “/users/home/username”).
+# NOTE that on a Joyent SmartMachine “JOY_DIR”
+# may be “/home/${JOY}” or similar, whereat on a
+# Shared SmartMachine it’d be “/users/home/${JOY}”.
+
+DATESTAMP=`date "+%Y-%m-%d"`
+JOY_DIR="/users/home/${JOY}"
+JOY_BACKUP_DIR="${JOY_DIR}/backup"
+
+
+# Remember the last rotated backup destination.
+
+if [ ! -f ${JOY_BACKUP_DIR}/${SS_NAME_1} ] && [ ! -f ${JOY_BACKUP_DIR}/${SS_NAME_2} ] && [ ! -f ${JOY_BACKUP_DIR}/${SS_NAME_3} ] && [ ! -f ${JOY_BACKUP_DIR}/${SS_NAME_4} ] && [ ! -f ${JOY_BACKUP_DIR}/${SS_NAME_5} ]; then
+	/usr/bin/touch ${JOY_BACKUP_DIR}/${SS_NAME_1}
+	SS_DIR="${SS_BASE}/${SS_NAME_1}"
+elif [ -f ${JOY_BACKUP_DIR}/${SS_NAME_1} ]; then
+	/usr/bin/mv ${JOY_BACKUP_DIR}/${SS_NAME_1} ${JOY_BACKUP_DIR}/${SS_NAME_2}
+	SS_DIR="${SS_BASE}/${SS_NAME_2}"
+elif [ -f ${JOY_BACKUP_DIR}/${SS_NAME_2} ]; then
+	/usr/bin/mv ${JOY_BACKUP_DIR}/${SS_NAME_2} ${JOY_BACKUP_DIR}/${SS_NAME_3}
+	SS_DIR="${SS_BASE}/${SS_NAME_3}"
+elif [ -f ${JOY_BACKUP_DIR}/${SS_NAME_3} ]; then
+	/usr/bin/mv ${JOY_BACKUP_DIR}/${SS_NAME_3} ${JOY_BACKUP_DIR}/${SS_NAME_4}
+	SS_DIR="${SS_BASE}/${SS_NAME_4}"
+elif [ -f ${JOY_BACKUP_DIR}/${SS_NAME_4} ]; then
+	/usr/bin/mv ${JOY_BACKUP_DIR}/${SS_NAME_4} ${JOY_BACKUP_DIR}/${SS_NAME_5}
+	SS_DIR="${SS_BASE}/${SS_NAME_5}"
+else 
+	/usr/bin/mv ${JOY_BACKUP_DIR}/${SS_NAME_5} ${JOY_BACKUP_DIR}/${SS_NAME_1}
+	SS_DIR="${SS_BASE}/${SS_NAME_1}"
+fi
+
+
+# Backup the MySQL databases.
+
+for db in ${MY_DB}; do
+	/usr/bin/mkdir -p ${JOY_BACKUP_DIR}/mysql/${db}
+	/usr/local/bin/mysqldump --user=${MY_UN} --password=${MY_PW} -Q --database ${db} > ${JOY_BACKUP_DIR}/mysql/${db}/${DATESTAMP}.sql
+	/usr/bin/find ${JOY_BACKUP_DIR}/mysql/${db}/*.sql -mtime +${MTIME} -exec /usr/bin/rm {} \;
+done
+
+
+# Run the backup.
+# NOTE that on a Joyent SmartMachine rsync
+# resides in “/opt/local/bin/rsync”, whereat on a
+# Shared SmartMachine it’s “/usr/local/bin/rsync”.
+
+/usr/local/bin/rsync -rltpqz --delete --delete-after --exclude-from "${JOY_BACKUP_DIR}/exclude.txt" ${JOY_DIR}/ ${SS_ALIAS}:${SS_DIR}
