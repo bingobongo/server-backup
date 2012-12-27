82867063df99861302dcd1be4a310ad8e1119171
Markus Mayer <onaneerde@doogvaard.net>
2012-12-27 12:38:14 +0100

    Major rewrite

--- README.md
+++ README.md
@@ -1,37 +1,76 @@
-Automated, rotating server backups to Strongspace
-=================================================
+Automated, rotating server backup to Strongspace
+================================================
 
-Kudos to **ExpanDrive**
 
-- [Password-less login to Strongspace with SSH Keys](https://www.strongspace.com/help/password-less-login-with-ssh-keys)
-- [Type less with a SSH Alias](https://www.strongspace.com/help/ssh-alias)
-- [Automated Offsite MySQL backup to Strongspace](https://www.strongspace.com/help/automated-offsite-mysql-backup)
+Package Overview
+----------------
 
-and **Joyent**
-
-- [Automated backups to Strongspace](http://oldwiki.joyent.com/shared:automated-backups)
+    /LICENSE
+    /README.md
+    /backup.sh
+    /config
+    /exclude-rsync.txt
+    /exclude-tar.txt
 
 
 Setup
 -----
 
-1. Create the directory where to store database dumps and the backup script on your server; set its permissons to `700`; e.g.
+1. Set up [password-less logins to Strongspace](https://www.strongspace.com/help/password-less-login-with-ssh-keys).  
+To use multiple keys, name the file in which to save the key (e.g. `strongspace_rsa`) and subsequently specify it as the identity file in `config`. For example enter the code below to generate a new SSH key named `strongspace_rsa`. After generating the keys it will prompt for a passphrase which will be used when accessing the key. Leave it blank when using it for scheduled rsync backup.
 
-		% mkdir -m 700 ~/backup
+        % ssh-keygen -f ~/.ssh/strongspace_rsa -t rsa
+    Set the permissions of the key pair, especially of the non-public key, to `600`.
 
-2. Create the directories that will be used for the backup rotation purpose on Strongspace; they must all be in that same Space.
+        % chmod 600 ~/.ssh/strongspace_rsa  ~/.ssh/strongspace_rsa.pub
+    Add the public key `strongspace_rsa.pub` to the authorized keys at Strongspace. Therefore, log into Strongspace, go to “Account” – “SSH Public Keys” – “Add a new key…” and paste the public key in there, finally save.
 
-3. Set up [password-less logins to Strongspace](https://www.strongspace.com/help/password-less-login-with-ssh-keys); set the permissions of the keys to `600`.  
-To use multiple keys, name the file in which to save the key e.g. `ss_rsa` and specify it as identity file in `config`.
+2. Create an SSH alias as done in `config` and described in [type less with a SSH alias](https://www.strongspace.com/help/ssh-alias). To test the setup, log into Strongspace using the command line `sftp` tool. While connecting to Strongspace it may prompt for continuation once, continue with `yes`. After a  successfull login, the command `bye` or `exit` logs out.
 
-4. Create an SSH alias as done in the `config` file and described [here](https://www.strongspace.com/help/ssh-alias).
+        % sftp ss
 
-5. Configure `backup.sh` and `exclude.txt`. Instructions on configuration are found inside `backup.sh`. Upload the two files to the before created directory on your server. Set the permissons of `backup.sh` to `700` and `exclude.txt` to `600`.
+3. Create the directory where to store database dumps and the backup script on your server, set its permissons to `700`, e.g.
 
-6. Test the script and check that it runs okay.
+        % mkdir -m 700 ~/backup
 
-		% ~/backup/backup.sh
+4. Configure `backup.sh`, `exclude-tar.txt` and `exclude-rsync.txt`, whereby the latter may be omitted if no synchronization with Strongspace is intended. Instructions on configuration are found inside `backup.sh`. Upload the files to the before created directory `~/backup` on your server. Set the permissons of `backup.sh` to `700` and the other two to `600`.  
+    To prevent possible errors on a TextDrive **Shared** or when not running as root (e.g. group/owner bubkis), add the line
+
+        `*/homes/db/Maildir`
+    to `exclude-tar.txt` and
+
+        `- homes/db/Maildir/`
+    to `exclude-rsync.txt`.  
+    The rest should be left as is.
+
+5. Create the directories that will be used for the backup rotation purpose on Strongspace; they must all be in that same Strongspace Space.
+
+6. Test the script and check that it runs okay.  
+    Use option `v` to display the messages instead of writing them to the log file `~/backup/backup.log`.  
+    With option `y` the backup is stored only locally without any synchronization with Strongspace.  
+    Option `L` resets the log file before writing to it and `D` deletes backups of unlisted databases, directories and files; i.e.
+
+        % ~/backup/backup.sh -vyLD
 
 7. Finally add it as a cronjob.
-	- On a Joyent **SmartMachine** log in to Webmin and navigate to <b>Webmin</b> – <b>System</b> – <b>Scheduled Cron Jobs</b>. There hit <b>Create a new scheduled cron job</b>. In the input box labelled <b>Execute cron job as</b> put `root` and in the input box labelled <b>Command</b> put the path to your backup script (e.g. `/home/USERNAME/backup/backup.sh`). Finally set when you want it to execute and hit <b>Create</b> to finish.
-	- On a Joyent **Shared SmartMachine** log in to Virtualmin and navigate to <b>Webmin Modules</b> – <b>Scheduled Cron Jobs</b>. There hit <b>Create a new scheduled cron job</b>. In the input box labelled <b>Command</b> put the path to your backup script (e.g. `/users/home/USERNAME/backup/backup.sh`). Finally set when you want it to execute and hit <b>Create</b> to finish.
+    - On a Joyent **SmartMachine Standard** use `crontab` with option `e` while logged in as root to schedule a backup.
+
+            % crontab -e
+        For a backup once a week, for example at 6:00 a.m. every monday, add in a new line:
+ 
+            0 6 * * 1 /full-pathname-to/backup/backup.sh
+        Save, quit and it should run automatically on the next scheduled date.
+    - On a Joyent **SmartMachine Plus** log into Webmin and navigate to “Webmin” – “System” – “Scheduled Cron Jobs”. There hit “Create a new scheduled cron job”. In the input box labelled “Execute cron job as” put `root` and in the input box labelled “Command” put the path to your backup script (dependent on its location, e.g. `/home/USERNAME/backup/backup.sh` or `/root/backup/backup.sh`). Finally set the day and time when you want it to execute and hit “Create” to finish.
+    - On a TextDrive **Shared** log into Virtualmin and navigate to “Webmin Modules” – “Scheduled Cron Jobs”. There hit “Create a new scheduled cron job”. In the input box labelled “Command” put the path to your backup script (e.g. `/users/home/USERNAME/backup/backup.sh`). Finally set the day and time when you want it to execute and hit “Create” to finish.
+
+
+License
+-------
+
+Read the `LICENSE` for license and copyright details.
+
+
+Credits
+-------
+
+Kudos to **ExpanDrive** ([Password-less login to Strongspace with SSH Keys](https://www.strongspace.com/help/password-less-login-with-ssh-keys), [Type less with a SSH Alias](https://www.strongspace.com/help/ssh-alias), [Automated Offsite MySQL backup to Strongspace](https://www.strongspace.com/help/automated-offsite-mysql-backup)) and **Joyent** ([Automated backups to Strongspace](http://oldwiki.joyent.com/shared:automated-backups)).
--- backup.sh
+++ backup.sh
@@ -1,97 +1,358 @@
-#!/bin/sh
+#!/usr/bin/bash
+#
+# Automated, rotating server backup to Strongspace
+#
+# @version    0.2
+# @author     Markus Mayer
+# @license    MIT License
+# @copyright  2012- Markus Mayer
+# @link       http://doogvaard.net/server-backup-rotation
 
 
 # CONFIGURATION SECTION
 # ---------------------
 
-# The primary user name that is used to login to the
-# (Shared) SmartMachine.
+# The pathname of the directory where the exclude-text-files
+# are stored and where to store backups, log, etc.
+# For example "/root/backup" or "/home/username/backup"
+# or similar, whereat on a TextDrive Shared it probably
+# would be "/users/home/username/backup". If its name
+# is different from "backup", align the patterns inside
+# "exclude-tar.txt" and, if not omitted, "exclude-rsync.txt"
+# accordingly.
 
-JOY=username
+DESTINATION="/pathname/backup"
 
+# A space-separated list of directory/file pathnames
+# to backup. On a TextDrive Shared this might be
+# "/users/home/username" or a subdirectory/file thereof.
 
-# The MySQL user name, password and a space-separated
-# list of MySQL database names to backup.
+PATHNAMES="/pathname/directory /pathname/file.ext"
 
-MY_UN=db-username
-MY_PW=db-password
-MY_DB='db-name-1 db-name-2'
-
-
-# The Strongspace SSH alias name
-# (i.e. the before defined Host in .ssh/config file).
-
-SS_ALIAS=ss
-
-
-# The base directory plus its five subdirectory names
-# (used for the rotation purpose) under which to store
-# the backup on Strongspace.
-
-SS_BASE=/strongspace/username/space-name
-SS_NAME_1=subdirectory-name-1
-SS_NAME_2=subdirectory-name-2
-SS_NAME_3=subdirectory-name-3
-SS_NAME_4=subdirectory-name-4
-SS_NAME_5=subdirectory-name-5
-
-
-# How many days to keep the database backups for;
-# e.g. for a backup once a week and wanting to keep
-# eight backups on the disk, set this to 56.
+# How many days to keep database dumps and tarballs for;
+# e.g. for a backup once a week and keeping eight backups
+# on the disk, set this to 56.
 
 MTIME=56
 
+# The MySQL user name, password and a space-separated
+# list of MySQL database names to backup; optionally.
 
-# DO NOT EDIT TOO MUCH BELOW HERE, UNLESS ONE IS DOING SOMETHING CUSTOM
-# ---------------------------------------------------------------------
+MYSQL_USER="db-username"
+MYSQL_PASSWD="db-password"
+MYSQL_NAMES="db-name-1 db-name-2"
 
+# The SSH alias name used for the connection to Strongspace,
+# i.e. the before defined Host in ~/.ssh/config file.
 
-# Define pathes (i.e. "/users/home/username").
-# NOTE that on a Joyent SmartMachine JOY_DIR
-# may be "/home/${JOY}" or similar, whereat on a
-# Shared SmartMachine it would be "/users/home/${JOY}".
+SS_ALIAS="ss"
 
-DATESTAMP=`date "+%Y-%m-%d"`
-JOY_DIR="/users/home/${JOY}"
-JOY_BACKUP_DIR="${JOY_DIR}/backup"
+# The pathname of the base directory on Strongspace,
+# i.e. the pathname of the Strongspace Space.
 
+SS_BASE="/strongspace/username/space-name"
 
-# Remember the last rotated backup destination.
+# A space-separated list of subdirectory names where to
+# store and rotate the backup in the Strongspace Space.
+# Note that the subdirectories must have been created
+# manually before running the backup the first time.
 
-if [ ! -f ${JOY_BACKUP_DIR}/${SS_NAME_1} ] && [ ! -f ${JOY_BACKUP_DIR}/${SS_NAME_2} ] && [ ! -f ${JOY_BACKUP_DIR}/${SS_NAME_3} ] && [ ! -f ${JOY_BACKUP_DIR}/${SS_NAME_4} ] && [ ! -f ${JOY_BACKUP_DIR}/${SS_NAME_5} ]; then
-	/usr/bin/touch ${JOY_BACKUP_DIR}/${SS_NAME_1}
-	SS_DIR="${SS_BASE}/${SS_NAME_1}"
-elif [ -f ${JOY_BACKUP_DIR}/${SS_NAME_1} ]; then
-	/usr/bin/mv ${JOY_BACKUP_DIR}/${SS_NAME_1} ${JOY_BACKUP_DIR}/${SS_NAME_2}
-	SS_DIR="${SS_BASE}/${SS_NAME_2}"
-elif [ -f ${JOY_BACKUP_DIR}/${SS_NAME_2} ]; then
-	/usr/bin/mv ${JOY_BACKUP_DIR}/${SS_NAME_2} ${JOY_BACKUP_DIR}/${SS_NAME_3}
-	SS_DIR="${SS_BASE}/${SS_NAME_3}"
-elif [ -f ${JOY_BACKUP_DIR}/${SS_NAME_3} ]; then
-	/usr/bin/mv ${JOY_BACKUP_DIR}/${SS_NAME_3} ${JOY_BACKUP_DIR}/${SS_NAME_4}
-	SS_DIR="${SS_BASE}/${SS_NAME_4}"
-elif [ -f ${JOY_BACKUP_DIR}/${SS_NAME_4} ]; then
-	/usr/bin/mv ${JOY_BACKUP_DIR}/${SS_NAME_4} ${JOY_BACKUP_DIR}/${SS_NAME_5}
-	SS_DIR="${SS_BASE}/${SS_NAME_5}"
-else 
-	/usr/bin/mv ${JOY_BACKUP_DIR}/${SS_NAME_5} ${JOY_BACKUP_DIR}/${SS_NAME_1}
-	SS_DIR="${SS_BASE}/${SS_NAME_1}"
-fi
+SS_DIRS="subdir-name-1 subdir-name-2"
+
+# The prefix of the backup destination mark for superb
+# readability and the basename used for naming database
+# dumps, tarballs. Can be left as is.
+
+SS_PREFIX="_recently_synced_with_"
+TIMESTAMP=`date "+%Y%m%d_%H%M%S"`
+
+# -----------------
+# CONFIGURATION END
 
 
-# Backup the MySQL databases.
+# DO NOT CHANGE TOO MUCH BELOW HERE,
+# UNLESS ONE IS DOING SOMETHING CUSTOM.
+# -------------------------------------
 
-for db in ${MY_DB}; do
-	/usr/bin/mkdir -p ${JOY_BACKUP_DIR}/mysql/${db}
-	/usr/local/bin/mysqldump --user=${MY_UN} --password=${MY_PW} -Q --database ${db} > ${JOY_BACKUP_DIR}/mysql/${db}/${DATESTAMP}.sql
-	/usr/bin/find ${JOY_BACKUP_DIR}/mysql/${db}/*.sql -mtime +${MTIME} -exec /usr/bin/rm {} \;
+# Note that the pathes to some utilities might
+# differ depending on the respective server.
+# Make sure that the correct pathes are noted.
+# This is particularly true for rsync, tar, mysqldump.
+
+# Sets log path for safty's sake.
+
+[ -d ${DESTINATION} ] && LOGFILE="${DESTINATION}/backup.log" || LOGFILE=/dev/null
+
+log()
+{
+  echo "${1}"
+  if [ -n "${2}" ]; then
+    cat << EOF
+  $(date "+%F %T")
+* -------------------
+EOF
+    exit "${2}"
+  fi
+}
+
+backup_mysql()
+{
+  [ -z "${1}" ] && return 1
+  if mkdir -m 700 -p ${DESTINATION}/mysql/${1} && \
+     /opt/local/bin/mysqldump --user=${MYSQL_USER} --password=${MYSQL_PASSWD} \
+       -Q --database ${1} > ${DESTINATION}/mysql/${1}/${TIMESTAMP}.sql
+  then
+    log "* Dumped MySQL database '${1}' to 'mysql/${1}/${TIMESTAMP}.sql'."
+  else
+    log "* ERROR - Failed to backup MySQL database '${1}'." && return 2
+  fi
+  remove_outdated ${DESTINATION}/mysql/${1}/*.sql
+}
+
+backup_pathname()
+{
+  [ -z "${1}" ] && return 1
+  FLAT=${1//\//_}; FLAT=${FLAT#_}
+  if mkdir -m 700 -p ${DESTINATION}/tar/${FLAT} && \
+     /opt/local/bin/gtar -pszcf ${DESTINATION}/tar/${FLAT}/${TIMESTAMP}.tar.gz \
+       --exclude-from ${DESTINATION}/exclude-tar.txt --warning none -C / ${1#/}
+  then
+    log "* Dumped '${1}' to 'tar/${FLAT}/${TIMESTAMP}.tar.gz'."
+  else
+    log "* ERROR - Failed to backup '${1}'." && return 2
+  fi
+  if [[ ! $LOCALONLY && -d ${1} && ! -d ${DESTINATION}/data/${FLAT} ]]; then
+    mkdir -m 700 -p ${DESTINATION}/data/${FLAT} && \
+      log "  Created placeholder 'data/${FLAT}'." || \
+      log "  WARNING - Failed to create placeholder for directory '${1}'."
+  fi
+  remove_outdated ${DESTINATION}/tar/${FLAT}/*.tar.gz
+}
+
+remove_outdated()
+{
+  [ -z "${1}" ] && return 1
+  OUTDATED=`find ${1} -mtime +${MTIME}`
+  if [ -n "${OUTDATED}" ]; then
+    rm -f ${OUTDATED} && log "  Removed outdated dumps." || \
+      log "  WARNING - Failed to remove outdated dumps."
+  fi
+}
+
+remove_unlisted()
+{
+  [[ -z "${1}" ||  ! -d "${DESTINATION}/${1}" || -z "${2}" ]] && return 1
+  LISTPATH="${DESTINATION}/${1}"; LIST="${2}"
+
+  # Flattens pathnames for comparison.
+
+  TEMP=""
+  for PATHNAME in ${LIST}; do
+    FLAT=${PATHNAME//\//_}; FLAT=${FLAT#_}
+    TEMP="${TEMP} ${FLAT}"
+  done
+  LIST=${TEMP# }; TEMP=""
+
+  for PATHNAME in ${LISTPATH}/*/; do
+    PATHNAME=${PATHNAME%*/}
+    NAME="${PATHNAME##*/}"
+    [[ ${NAME} == "*" ]] && continue
+    is_listed ${NAME} "${LIST}"
+    if [ $? -gt 1 ]; then
+      rm -rf ${PATHNAME} && log "* Removed unlisted '${1}/${NAME}'." || \
+        log "* WARNING - Failed to remove unlisted '${1}/${NAME}'."
+    fi
+  done
+}
+
+is_listed()
+{
+  [[ -z "${1}" || -z "${2}" ]] && return 1
+  for LISTED in ${2}; do
+    [[ ${LISTED} == ${1} ]] && return
+  done
+  return 2
+}
+
+sync_pathname()
+{
+  [[ -z "${1}" || ! -d ${1} ]] && return 1
+  FLAT=${1//\//_}; FLAT=${FLAT#_}
+  if [ ! -d ${DESTINATION}/data/${FLAT} ]; then
+    log "* ERROR - No placeholder found for '${1}', synchronization abandoned." && return 2
+  elif /opt/local/bin/rsync ${RSYNC_OPTION} --delete --delete-after \
+         ${EXCLUDE_FROM_RSYNC} ${1}/ ${SS_ALIAS}:${SS_PATH}/data/${FLAT}
+  then
+    log "* Synced directory '${1}'."
+  else
+    log "* ERROR - Failed to synchronize directory '${1}'." && return 2
+  fi
+}
+
+run_backup()
+{
+  # Empties the log file.
+
+  [[ $LOGRESET && -f "${LOGFILE}" ]] && $(> "${LOGFILE}") && log "* Emptied log file '${LOGFILE}'."
+
+  USERNAME="$(id -un)"
+  if [ "${USERNAME}" != "root" ]; then
+    cat << EOF
+* WARNING - Not running as root.
+  Running as ${USERNAME} seems right, though it might be wrong.
+  Besides, rsync executes with options '-rlptzq' instead of '-azq'.
+EOF
+    RSYNC_OPTION="-rlptzq"
+  else
+    RSYNC_OPTION="-azq"
+  fi
+
+  # Verifies destination path.
+
+  [ ! -e ${DESTINATION} ] && \
+    log "* ERROR - DESTINATION '${DESTINATION}' doesn't exist." 2
+  [ ! -d ${DESTINATION} ] && \
+    log "* ERROR - DESTINATION '${DESTINATION}' is not a directory." 2
+
+  # Prevents from configuration bubkis.
+
+  if [[ ! $LOCALONLY ]]; then
+
+    # Seeks for recently used subdirectory name.
+
+    for NAME in ${SS_DIRS}; do
+      [ -z "${FUTURE}" ] && [ ! -f ${DESTINATION}/${SS_PREFIX}${NAME} ] && FUTURE=${NAME}
+      if [ -z "${RECENT}" ]; then
+        [ -f ${DESTINATION}/${SS_PREFIX}${NAME} ] && RECENT=${NAME}
+        continue
+      else
+        FUTURE=${NAME}; break
+      fi
+    done
+
+    [ -z "${SS_ALIAS}" ] && \
+      log "* ERROR - Undefined SS_ALIAS." 2
+    [[ -z "${SS_BASE}" || "${SS_BASE}" == "/strongspace/username/space-name" ]] && \
+      log "* ERROR - SS_BASE '${SS_BASE}' is not advisable." 2
+    [[ -z "${SS_DIRS}" || "${SS_DIRS}" == "subdir-name-1 subdir-name-2" ]] && \
+      log "* ERROR - SS_DIRS '${SS_DIRS}' is not advisable." 2
+    [ -z "${FUTURE}" ] && \
+      log "* ERROR - Undefined FUTURE, failed to set SS_PATH." 2
+    [ -f ${DESTINATION}/exclude-rsync.txt ] && \
+      EXCLUDE_FROM_RSYNC="--exclude-from ${DESTINATION}/exclude-rsync.txt"
+    SS_PATH="${SS_BASE}/${FUTURE}" # Sets full Strongspace pathname.
+  fi
+
+  # Verifies existence of pathnames.
+
+  TEMP=""
+  for PATHNAME in ${PATHNAMES}; do
+    [ ! -e ${PATHNAME} ] && log "* WARNING - '${PATHNAME}' doesn't exist." && continue
+    TEMP="${TEMP} ${PATHNAME}"
+  done
+  PATHNAMES=${TEMP# }; TEMP=""
+
+  # Makes backup of directory, file.
+
+  if [ -z "${PATHNAMES}" ]; then
+    log "* WARNING - No PATHNAMES found, its backup abandoned."
+  else
+
+    # Verifies existence of exclude-tar.txt.
+
+    [ ! -f ${DESTINATION}/exclude-tar.txt ] && \
+      log "* ERROR - File '${DESTINATION}/exclude-tar.txt' doesn't exist." 2
+
+    for PATHNAME in ${PATHNAMES}; do
+      backup_pathname ${PATHNAME}
+    done
+  fi
+
+  # Makes backup of database.
+
+  if [[ -z "${MYSQL_USER}"   || "${MYSQL_USER}" == "db-username" || \
+        -z "${MYSQL_PASSWD}" || "${MYSQL_PASSWD}" == "db-password" || \
+        -z "${MYSQL_NAMES}"  || "${MYSQL_NAMES}" == "db-name-1 db-name-2" \
+  ]]; then
+    log "* WARNING - MySQL setup is not advisable, its backup abandoned."
+  else
+    for NAME in ${MYSQL_NAMES}; do
+      backup_mysql ${NAME}
+    done
+  fi
+
+  # Removes backup of unlisted database, directory, file.
+
+  if [[ $DELETE_UNLISTED ]]; then
+    remove_unlisted "tar" "${PATHNAMES}"
+    remove_unlisted "data" "${PATHNAMES}"
+    remove_unlisted "mysql" "${MYSQL_NAMES}"
+  fi
+
+  if [[ ! $LOCALONLY ]]; then
+
+    # Synchronizes the backup.
+    # To test rsync manually with progress and stats (note to replace
+    # options "-azq" with "-rlptzq" when not running as root), e.g.
+    # rsync -azv --delete --delete-after --stats --progress /DESTINATION/backup/ ss:/strongspace/USERNAME/SPACE-NAME/SUBDIRECTORY-NAME
+
+    if /opt/local/bin/rsync ${RSYNC_OPTION} --delete --delete-after ${DESTINATION}/ ${SS_ALIAS}:${SS_PATH}; then
+      log "* Synced directory '${DESTINATION}'."
+    else
+      log "* ERROR - Failed to synchronize directory '${DESTINATION}'." 2
+    fi
+
+    # Remembers the last rotated backup destination.
+
+    if [ -n "${RECENT}" ] && [ -n "${FUTURE}" ]; then
+      if mv ${DESTINATION}/${SS_PREFIX}${RECENT} ${DESTINATION}/${SS_PREFIX}${FUTURE}; then
+        log "* Updated destination mark '${SS_PREFIX}${RECENT}' to '${FUTURE}'."
+      else
+        log "* ERROR - Failed to update destination mark '${SS_PREFIX}${RECENT}'." 2
+      fi
+    else
+      if touch ${DESTINATION}/${SS_PREFIX}${FUTURE}; then
+        log "* Created destination mark '${SS_PREFIX}${FUTURE}'."
+      else
+        log "* ERROR - Failed to create destination mark '${SS_PREFIX}${FUTURE}'." 2
+      fi
+    fi
+
+    # Pathname synchronization comes last.
+
+    if [ -n "${PATHNAMES}" ]; then
+      for PATHNAME in ${PATHNAMES}; do
+        sync_pathname ${PATHNAME}
+      done
+    fi
+  fi
+
+  log "* Finished, m'Lord." 0
+}
+
+# Checks if verbose mode and/or local-only (without rsync) is turned on,
+# and if the log file should be emptied and an unlisted backup be removed.
+
+while getopts ":vyLD" OPTION; do
+  case $OPTION in
+    v)
+      VERBOSE="verbose";
+      ;;
+    y)
+      LOCALONLY="local-only";
+      ;;
+    L)
+      LOGRESET="reset-log";
+      ;;
+    D)
+      DELETE_UNLISTED="delete-unlisted";
+      ;;
+    *)
+      exit 2;
+      ;;
+  esac
 done
 
+# Runs the backup.
 
-# Run the backup.
-# NOTE that on a Joyent SmartMachine rsync
-# resides in "/opt/local/bin/rsync", whereat on a
-# Shared SmartMachine it is "/usr/local/bin/rsync".
-
-/usr/local/bin/rsync -rltpqz --delete --delete-after --exclude-from "${JOY_BACKUP_DIR}/exclude.txt" ${JOY_DIR}/ ${SS_ALIAS}:${SS_DIR}
+[[ ! $VERBOSE ]] && run_backup 2>&1 >> ${LOGFILE} || run_backup
